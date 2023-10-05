713bc9eb65e5dc610cc8e34a061f8bbc73221ff9
Somdev Das <47299585+imsomdev@users.noreply.github.com>
2023-10-06 00:17:48 +0530

    Time: 2 ms (95.89%) | Memory: 43.4 MB (56.74%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,44 +1,26 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> resultSet = new HashSet<>();
 
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        // Add all numbers from nums1 to set1
+        for (int num : nums1) {
+            set1.add(num);
+        }
 
-        int i = 0, j = 0;
-        ArrayList<Integer> resultList = new ArrayList<>();
-
-        while (i < nums1.length && j < nums2.length) {
-            // Skip duplicates in nums1
-            while (i > 0 && i < nums1.length && nums1[i] == nums1[i - 1]) {
-                i++;
-            }
-
-            // Skip duplicates in nums2
-            while (j > 0 && j < nums2.length && nums2[j] == nums2[j - 1]) {
-                j++;
-            }
-
-            // If the current elements are equal, add to result and move both pointers
-            if (i < nums1.length && j < nums2.length && nums1[i] == nums2[j]) {
-                resultList.add(nums1[i]);
-                i++;
-                j++;
-            }
-            // If current element in nums1 is less, move its pointer
-            else if (i < nums1.length && j < nums2.length && nums1[i] < nums2[j]) {
-                i++;
-            }
-            // If current element in nums2 is less, move its pointer
-            else if (i < nums1.length && j < nums2.length) {
-                j++;
+        // Check if any number from nums2 exists in set1
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                resultSet.add(num);
             }
         }
 
-        int[] result = new int[resultList.size()];
-        for (int k = 0; k < resultList.size(); k++) {
-            result[k] = resultList.get(k);
+        // Convert resultSet to array
+        int[] result = new int[resultSet.size()];
+        int i = 0;
+        for (int num : resultSet) {
+            result[i++] = num;
         }
-
         return result;
     }
 }
