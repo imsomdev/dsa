4a0541325ffe9dda9c41b0dc7856e9aa31351887
Somdev Das <47299585+imsomdev@users.noreply.github.com>
2023-10-28 19:35:58 +0530

    Time: 16 ms (41.52%) | Memory: 13.2 MB (56.53%) - LeetSync

--- 812-rotate-string/rotate-string.py
+++ 812-rotate-string/rotate-string.py
@@ -2,7 +2,10 @@ class Solution(object):
     def rotateString(self, s, goal):
         if len(s) != len(goal):
             return False
-            
+        
+        if sorted(s) != sorted(goal):
+            return False
+
         return goal in s + s
                     
     
