35da76d7ccb249ecb1448b0ccabd1daace58ec44
Somdev Das <47299585+imsomdev@users.noreply.github.com>
2024-01-10 08:34:39 +0530

    Time: 64 ms (28.72%) | Memory: 52.8 MB (5.17%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -0,0 +1,31 @@
+class Solution {
+    private static final int MOD = 1_000_000_007;
+
+    public int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        long sum = 0;
+        Stack<Integer> stack = new Stack<>();
+        int[] left = new int[n];
+        int[] right = new int[n];
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[i] < arr[stack.peek()]) {
+                stack.pop();
+            }
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        stack.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[i] <= arr[stack.peek()]) {
+                stack.pop();
+            }
+            right[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+        for (int i = 0; i < n; i++) {
+            sum = (sum + (long) arr[i] * (i - left[i]) * (right[i] - i)) % MOD;
+        }
+
+        return (int) sum;
+    }
+}
