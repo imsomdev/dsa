7b7507b7cf301033eaba9467c46faeb7386e6308
Somdev Das <47299585+imsomdev@users.noreply.github.com>
2024-01-22 00:31:43 +0530

    Time: 25 ms (50.35%) | Memory: 46.1 MB (73.84%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,27 +1,16 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        // Map to store the frequency of prefix sums
-        Map<Integer, Integer> prefixSumFrequency = new HashMap<>();
-        
-        int prefixSum = 0; 
-        int count = 0; // To store the count of subarrays with sum = k
-        
-        // To handle cases where prefixSum is exactly k
-        prefixSumFrequency.put(0, 1);
-        
-        for (int i = 0; i < nums.length; i++) {
-            prefixSum += nums[i];
-            
-            // If there are any subarrays ending at i with sum = k,
-            // then we add the count of such subarrays to our result.
-            if (prefixSumFrequency.containsKey(prefixSum - k)) {
-                count += prefixSumFrequency.get(prefixSum - k);
-            }
-            
-            // Update the prefixSumFrequency map
-            prefixSumFrequency.put(prefixSum, prefixSumFrequency.getOrDefault(prefixSum, 0) + 1);
+        int count = 0;
+        int sum = 0;
+        Map<Integer, Integer> sumOccurrences = new HashMap<>();
+        sumOccurrences.put(0, 1);
+
+        for (int num : nums) {
+            sum += num;
+            count += sumOccurrences.getOrDefault(sum - k, 0);
+            sumOccurrences.put(sum, sumOccurrences.getOrDefault(sum, 0) + 1);
         }
-        
+
         return count;
     }
 }
