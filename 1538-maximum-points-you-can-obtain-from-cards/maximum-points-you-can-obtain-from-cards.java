cd0d954a58bf0becb9923314232be8a27fbffaa6
Somdev Das <47299585+imsomdev@users.noreply.github.com>
2024-02-03 22:50:16 +0530

    Time: 2 ms (99.13%) | Memory: 54.7 MB (88.07%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -1,29 +1,19 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
         int n = cardPoints.length;
-        int sum = 0;
-        int startSum = 0;
-        int endSum = 0;
-        int res = 0;
-        int start = 0;
-        int end = n - 1;
-        if(k == n){
-            for(int i=0; i < n; i++){
-                sum += cardPoints[i];
-            }
-            return sum;
+        int totalSum = 0;
+        int windowSum = 0;
+        for (int card : cardPoints) {
+            totalSum += card;
         }
-        for(int i = end; i > end - k; i--){
-            endSum += cardPoints[i];
+        for (int i = 0; i < n - k; i++) {
+            windowSum += cardPoints[i];
         }
-        for(int i = start; i < k; i++){
-            startSum += cardPoints[i];
-            for(int j = end; j > end - (k - (i+1)); j--){
-                sum += cardPoints[j];
-            }
-            res = Math.max(startSum + sum, res);
-            sum = 0;
+        int minWindowSum = windowSum;
+        for (int i = n - k; i < n; i++) {
+            windowSum += cardPoints[i] - cardPoints[i - (n - k)];
+            minWindowSum = Math.min(minWindowSum, windowSum);
         }
-        return Math.max(endSum, res);
+        return totalSum - minWindowSum;
     }
 }
