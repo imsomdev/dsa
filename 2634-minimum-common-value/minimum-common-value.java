8aebf590f52cc515a464bff3a9804779b54c740d
Somdev Das <47299585+imsomdev@users.noreply.github.com>
2024-03-09 18:50:22 +0530

    Time: 12 ms (28.67%) | Memory: 64 MB (13.78%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,27 +1,14 @@
 class Solution {
-    private boolean isThere(int[] arr, int num){
-    int start = 0;
-    int end = arr.length - 1;
-    while(start <= end){
-        int mid = (start + end) / 2;
-        if(num == arr[mid]){
-            return true;
-        }
-        else if(num > arr[mid]){
-            start = mid + 1;
-        }
-        else{
-            end = mid - 1;
-        }
-    }
-    return false;
-}
     public int getCommon(int[] nums1, int[] nums2) {
-        for(int i=0; i< nums1.length; i++){
-            if(isThere(nums2, nums1[i])){
-                return nums1[i];
+        HashSet<Integer> hs = new HashSet<>();
+        for(int num: nums1){
+            hs.add(num);
+        }
+        for(int num: nums2){
+            if(hs.contains(num)){
+                return num;
             }
         }
-        return  -1;
+        return -1;
     }
 }
