a6ca582902e362c307d303e4dcbe93561187b8d1
Somdev Das <47299585+imsomdev@users.noreply.github.com>
2024-04-03 11:12:56 +0530

    Time: 1 ms (75.05%) | Memory: 42 MB (53.83%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -1,56 +1,32 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    private List<List<Integer>> getList(TreeNode root) {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        if (root == null) {
+        if(root == null){
             return result;
         }
-        
-        Queue<TreeNode> q = new LinkedList<>();
+        boolean flag = true;
+        Queue <TreeNode> q = new LinkedList<>();
         q.offer(root);
-        boolean zigzag = false;
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> level = new ArrayList<>();
-
-            for (int i = 0; i < size; i++) {
+        while(!q.isEmpty()){
+            int level = q.size();
+            List<Integer> temp = new ArrayList<>();
+            for(int i=0; i<level; i++){
                 TreeNode currentNode = q.poll();
-                level.add(currentNode.val);
+                temp.add(currentNode.val);
 
-                if (currentNode.left != null) {
+                if(currentNode.left != null){
                     q.offer(currentNode.left);
                 }
-                if (currentNode.right != null) {
+                if(currentNode.right != null){
                     q.offer(currentNode.right);
                 }
             }
-
-            if (zigzag) {
-                Collections.reverse(level);
+            if(!flag){
+                Collections.reverse(temp);
             }
-            result.add(level);
-            zigzag = !zigzag;
+            result.add(temp);
+            flag = !flag;
         }
-        
         return result;
-    }
-
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        return getList(root);
     }
 }
