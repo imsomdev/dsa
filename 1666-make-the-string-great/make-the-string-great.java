a1aa695b41a9b5fc2b5a756d1755d1dec3dc43eb
Somdev Das <47299585+imsomdev@users.noreply.github.com>
2024-04-05 10:30:48 +0530

    Time: 2 ms (88.33%) | Memory: 41.8 MB (84.95%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -1,11 +1,20 @@
 class Solution {
     public String makeGood(String s) {
-        for (int i = 1; i < s.length(); i++) {
-            if (Math.abs(s.charAt(i) - s.charAt(i - 1)) == 32) {
-                s = s.substring(0, i - 1) + s.substring(i + 1);
-                i = 0;
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && Math.abs(stack.peek() - c) == 32) {
+                stack.pop();
+            } else {
+                stack.push(c);
             }
         }
-        return s;
+        
+        StringBuilder result = new StringBuilder();
+        for (char c : stack) {
+            result.append(c);
+        }
+        
+        return result.toString();
     }
 }
