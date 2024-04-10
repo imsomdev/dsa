df8f3fe8cd7ae3164d6412d848f166fc6710ec36
Somdev Das <47299585+imsomdev@users.noreply.github.com>
2024-04-10 11:21:42 +0530

    Time: 4 ms (86.41%) | Memory: 44.5 MB (7.28%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
@@ -1,21 +1,19 @@
 class Solution {
-  public int[] deckRevealedIncreasing(int[] deck) {
-    final int n = deck.length;
-
-    Arrays.sort(deck);
-
-    Deque<Integer> dq = new ArrayDeque<>();
-    dq.addFirst(deck[n - 1]);
-
-    for (int i = n - 2; i >= 0; --i) {
-      dq.addFirst(dq.getLast());
-      dq.pollLast();
-      dq.addFirst(deck[i]);
+    public int[] deckRevealedIncreasing(int[] deck) {
+        int n = deck.length;
+        Arrays.sort(deck);
+        
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            q.add(i);
+        }
+        
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            result[q.poll()] = deck[i];
+            q.add(q.poll());
+        }
+        
+        return result;
     }
-
-    for (int i = 0; i < n; ++i)
-      deck[i] = dq.pollFirst();
-
-    return deck;
-  }
 }
