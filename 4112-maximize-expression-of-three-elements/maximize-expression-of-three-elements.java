6608bb4dadc4e03bb647e90e9b79858ba56d82de
Somdev Das <47299585+imsomdev@users.noreply.github.com>
2025-11-21 16:58:00 +0530

    Time: 1 ms (99.82%) | Memory: 45.1 MB (80.19%) - LeetSync

---
+++ 4112-maximize-expression-of-three-elements/maximize-expression-of-three-elements.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maximizeExpressionOfThree(int[] nums) {
+        int a = Integer.MIN_VALUE;
+        int b = Integer.MIN_VALUE;
+        int c = Integer.MAX_VALUE;;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] > a){
+                b = a;
+                a = nums[i];
+            }
+            else if(nums[i] <= a && nums[i] > b){
+                b = nums[i];
+            }
+            if(nums[i] <c){
+                c = nums[i];
+            }
+        }
+        return (a+b)-c;
+    }
+}
