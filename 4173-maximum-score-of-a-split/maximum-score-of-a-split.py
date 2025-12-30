5f552d96aeca0fdb2ea59f1e5f09cb3448c25c46
Somdev Das <47299585+imsomdev@users.noreply.github.com>
2025-12-30 22:38:47 +0530

    Time: 240 ms (89.15%) | Memory: 22.6 MB (88.46%) - LeetSync

---
+++ 4173-maximum-score-of-a-split/maximum-score-of-a-split.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def maximumScore(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n < 2:
+            return 0
+        
+        suffix_min = [0] * n
+        suffix_min[-1] = nums[-1]
+        
+        for i in range(n - 2, -1, -1):
+            suffix_min[i] = min(nums[i], suffix_min[i+1])
+        
+        max_score = float('-inf')
+        current_prefix_sum = 0
+        
+        for i in range(n - 1):
+            current_prefix_sum += nums[i]
+            current_score = current_prefix_sum - suffix_min[i + 1]
+            if current_score > max_score:
+                max_score = current_score
+                
+        return max_score
