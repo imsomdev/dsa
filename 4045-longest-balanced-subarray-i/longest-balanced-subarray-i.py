9429270b7e0c54dd26a79f92f619703d2de46d05
Somdev Das <47299585+imsomdev@users.noreply.github.com>
2026-02-10 09:08:04 +0530

    Time: 2130 ms (47.06%) | Memory: 12.9 MB (5.88%) - LeetSync

---
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestBalanced(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        res = 0
+
+        for i in range(n):
+            even_set = set()
+            odd_set = set()
+
+            for j in range(i, n):
+                if nums[j] % 2 == 0:
+                    even_set.add(nums[j])
+                else:
+                    odd_set.add(nums[j])
+
+                if len(even_set) == len(odd_set):
+                    res = max(res, j - i + 1)
+
+        return res
