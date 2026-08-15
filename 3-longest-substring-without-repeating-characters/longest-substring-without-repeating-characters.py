c10fbe4f1226040c5702f91efa4e28714037c442
Somdev Das <47299585+imsomdev@users.noreply.github.com>
2026-08-15 23:30:43 +0530

    Time: 509 ms (5.01%) | Memory: 20.3 MB (11.64%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -6,16 +6,22 @@ class Solution:
         start = 0
 
         for c in s:
-            if c in items:
-                while True:
-                    if c == s[start]:
-                        items.remove(s[start])
-                        start += 1
-                        count -= 1
-                        break
-                    items.remove(s[start])
-                    start += 1
-                    count -= 1
+            # if c in items:
+            #     while True:
+            #         if c == s[start]:
+            #             items.remove(s[start])
+            #             start += 1
+            #             count -= 1
+            #             break
+            #         items.remove(s[start])
+            #         start += 1
+            #         count -= 1
+            #the above one also correct
+            
+            while c in items:
+                items.remove(s[start])
+                start += 1
+                count -= 1
 
             items.add(c)
             count += 1
