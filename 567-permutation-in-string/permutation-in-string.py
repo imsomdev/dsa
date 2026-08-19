45f495c7d54e77e6837ed8af3b05f970f070db3e
Somdev Das <47299585+imsomdev@users.noreply.github.com>
2026-08-19 08:02:43 +0530

    Time: 71 ms (20.97%) | Memory: 19.4 MB (53.49%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,14 +1,21 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1_sorted = sorted(s1)
-
-        start = 0
+        s1_count = Counter(s1)
+        window_count = Counter()
+        l = 0
         
-
         for i in range(len(s2)):
-            end = i + len(s1)
-            temp_window = s2[i:end]
-            if s1_sorted == sorted(temp_window):
+            window_count[s2[i]] += 1
+
+            if i - l + 1 > len(s1):
+                window_count[s2[l]] -= 1
+
+                if  window_count[s2[l]] == 0:
+                    del window_count[s2[l]]
+            
+                l +=1
+            
+            if s1_count == window_count:
                 return True
-        
+                
         return False
