24e7a5ea4017c33c48c3240dd77ce5c40e3c59de
Somdev Das <47299585+imsomdev@users.noreply.github.com>
2026-09-08 22:52:49 +0530

    Time: 152 ms (21.31%) | Memory: 19.8 MB (16.06%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,20 +1,16 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        frq = {}
-        max_frq = 0
+        freq = {}
+        max_freq = 0
         res = 0
 
         l = 0
-        for r in range(len(s)):
-            char = s[r]
-            frq[char] = frq.get(char, 0) + 1
-            max_frq = max(max_frq, frq[char])
+        for r, c in enumerate(s):
+            freq[c] = freq.get(c, 0) + 1
 
-            while (r - l + 1) - max_frq > k:
-                frq[s[l]] -= 1
+            while (r - l + 1) - max(freq.values()) > k:
+                freq[s[l]] -= 1
                 l += 1
+            res = max(res, (r - l + 1))
 
-            res = max(res, r - l + 1)
-
-        return res 
-
+        return res
